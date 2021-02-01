608202c41c4edfc64187463100652caaf8ecbd75
tycarry <2501160661@qq.com>
2021-02-02 00:56:07 +0800

    update

--- swiper/urls.py
+++ swiper/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api')
+    url(r'^api/user/$', include(('user.urls', 'user'), namespace='user')),
+
 
 ]
--- user/urls.py
+++ user/urls.py
@@ -4,10 +4,21 @@
 # @IDE: PyCharm
 # @Create time: 2/1/21 4:12 PM
 # @Description:
+from user import views
+
+# urlpatterns = [
+#     # url(r'^verify_code$', views.get_verify_code),
+#     url(r'^api/user/verify_code$', views.get_verify_code),
+#     url(r'^api/user/login$', user.login),
+#     url(r'^api/user/profile/show$', user.show_profile),
+#     url(r'^api/user/profile/modify$', user.modify_profile),
+#     url(r'^api/user/avatar/upload$', user.upload_avatar),
+# ]
+
 urlpatterns = [
-    url(r'^api/user/verify_code$', user.get_verify_code),
-    url(r'^api/user/login$', user.login),
-    url(r'^api/user/profile/show$', user.show_profile),
-    url(r'^api/user/profile/modify$', user.modify_profile),
-    url(r'^api/user/avatar/upload$', user.upload_avatar),
+    url(r'^verify_code$', views.get_verify_code, name='get_verify_code'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^profile/show$', views.show_profile, name='show_profile'),
+    url(r'^profile/modify$', views.modify_profile, name='modify_profile'),
+    url(r'^avatar/upload$', views.upload_avatar, name='upload_avatar'),
 ]
