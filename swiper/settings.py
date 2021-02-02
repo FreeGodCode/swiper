4da464f8866f65c0868282debaa1c4a2efb19a7a
tycarry <2501160661@qq.com>
2021-02-02 11:38:51 +0800

    update

--- swiper/settings.py
+++ swiper/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+# /home/ty/Projects/Django/swiper
 
 
 # Quick-start development settings - unsuitable for production
