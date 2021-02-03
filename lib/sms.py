dab599fcd4e1c7b6b43f17ed5a8bed7148de2bce
tycarry <2501160661@qq.com>
2021-02-04 01:33:01 +0800

    将发送短信设置为异步执行,发送验证码按照代码执行

--- lib/sms.py
+++ lib/sms.py
@@ -25,6 +25,16 @@ def gen_verify_code(length=6):
 
 
 @call_by_worker
+def send_sms(phone_num, msg):
+    """发送短信"""
+    # 复制一个HY_SMS_PARAMS,对其进行赋值操作,避免多人修改同一个
+    params = config.HY_SMS_PARAMS.copy()
+    params['mobile'] = phone_num
+    params['content'] = params['content'] % msg
+    response = requests.post(config.HY_SMS_URL, data=params)
+    return response
+
+
 def send_verify_code(phone_num):
     """
     #对接第三方的平台,发送验证码
@@ -33,17 +43,18 @@ def send_verify_code(phone_num):
     # 检查验证码是否过期, 过期了就重新发送
     if not cache.has_key(key):
         verify_code = gen_verify_code()
-        # 复制一个HY_SMS_PARAMS,对其进行赋值操作,避免多人修改同一个
-        params = config.HY_SMS_PARAMS.copy()
-        params['mobile'] = phone_num
-        params['content'] = params['content'] % verify_code
-        response = requests.post(config.HY_SMS_URL, data=params)
+        # params = config.HY_SMS_PARAMS.copy()
+        # params['mobile'] = phone_num
+        # params['content'] = params['content'] % verify_code
+        # response = requests.post(config.HY_SMS_URL, data=params)
+        send_sms(phone_num, verify_code)
         # 缓存
-        cache.set(key, verify_code, 90)  # timeout
+        cache.set(key, verify_code, 180)  # timeout
         return response
     else:
         from common.code import VcodeExist
         raise VcodeExist  # 验证码未过期
+
 
 # 装饰器
 # @celery_app.task
