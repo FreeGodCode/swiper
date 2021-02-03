731cff139cb37e9cf0ba5ef652deff0fda91d092
tycarry <2501160661@qq.com>
2021-02-03 12:08:39 +0800

    增加七牛云接入配置

--- swiper/config.py
+++ swiper/config.py
@@ -15,3 +15,8 @@ HY_SMS_PARAMS = {
     'format': 'json',
 }
 
+# 七牛云配置
+QN_BUCKET = 'swiper_space'
+QN_BASE_URL = 'http://qnwvltezo.hn-bkt.clouddn.com'
+QN_ACCESS_KEY = 'rAl8TwaBg3dzgBZx2qyEV9ap9UbVy7k5vioqRBKR'
+QN_SECRET_KEY = 'Yy0AfVCsdM4hvvu3Wa1mpWtDv7-IEvEcY0BE8uyJ'
