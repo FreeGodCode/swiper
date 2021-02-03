1fb6bfad7da272afc70f61a6d0f90d6cc9726137
tycarry <2501160661@qq.com>
2021-02-03 12:09:26 +0800

    添加本地缓存配置

--- swiper/settings.py
+++ swiper/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 缓存配置
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'  # 本地缓存
+    }
+}
