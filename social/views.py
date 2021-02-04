31cf13dd4690ba32390f43a07d850c6b67cb9099
tycarry <2501160661@qq.com>
2021-02-04 16:12:22 +0800

    初始化相关API

--- social/views.py
+++ social/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from lib.http import render_json
+
 
 # Create your views here.
+def index(request):
+    return render_json(None)
+
+
+def get_recommend_users(request):
+    """获取推荐列表"""
+    return render_json(None)
+
+
+def like(request):
+    """喜欢"""
+    return render_json(None)
+
+
+def dislike(request):
+    """不喜欢"""
+    return render_json(None)
+
+
+def super_like(request):
+    """超喜欢"""
+    return render_json(None)
+
+
+def regret(request):
+    """反悔"""
+    return render_json(None)
+
+
+def show_liked_me(request):
+    """显示喜欢我的"""
+    return render_json(None)
