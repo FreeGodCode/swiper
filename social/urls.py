19e08fd41962849cb85906b8ae8301735825a649
tycarry <2501160661@qq.com>
2021-02-04 16:11:38 +0800

    添加相关社交相关路由地址

--- social/urls.py
+++ social/urls.py
@@ -4,3 +4,16 @@
 # @IDE: PyCharm
 # @Create time: 2/4/21 3:41 PM
 # @Description:
+from django.conf.urls import url
+
+from social import views
+
+urlpatterns = [
+    url(r'/', views.index, name='index'),
+    url(r'^get_recommend_users', views.get_recommend_users, name='get_recommend_users'),
+    url(r'^like', views.like, name='like'),
+    url(r'^dislike', views.dislike, name='dislike'),
+    url(r'^super_like', views.super_like, name='super_like'),
+    url(r'^regret', views.regret, name='regret'),
+    url(r'^show_liked_me', views.show_liked_me, name='show_liked_me'),
+]
