4b42bea00d7abe4bfca8f988852d678c76986e8c
tycarry <2501160661@qq.com>
2021-02-04 19:10:28 +0800

    update

--- script/init.py
+++ script/init.py
@@ -106,6 +106,6 @@ def create_vip_permission_relations():
 
 if __name__ == '__main__':
     create_robots(1000)
-    init_permission()
-    init_vip()
-    create_vip_permission_relations()
+    # init_permission()
+    # init_vip()
+    # create_vip_permission_relations()
