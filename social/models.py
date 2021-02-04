4fa320cc9d7a46ca2d62136471110a4e53522695
tycarry <2501160661@qq.com>
2021-02-04 16:10:18 +0800

    添加社交相关的模型类

--- social/models.py
+++ social/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Swiped(models.Model):
+    """滑动记录表"""
+    FLAGS = (
+        ('superlike', '上滑'),
+        ('like', '右滑'),
+        ('dislike', '左滑'),
+    )
+    uid = models.IntegerField(verbose_name='滑动者的ID')
+    sid = models.IntegerField(verbose_name='被滑动者的ID')
+    flag = models.CharField(max_length=10, choices=FLAGS)  # 滑动标志
+    date_time = models.DateTimeField(auto_now=True)  # 滑动的时间
+
+
+class Friend(models.Model):
+    """好友关系表"""
+    # 好友关系是相互的
+    uid1 = models.IntegerField()
+    uid2 = models.IntegerField()
