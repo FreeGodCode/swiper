fb0b3df95163488a11962491e677b52086da9a69
tycarry <2501160661@qq.com>
2021-02-08 12:56:18 +0800

    添加权限检查装饰器

--- vip/helper.py
+++ vip/helper.py
@@ -3,4 +3,26 @@
 # @File name: helper.py 
 # @IDE: PyCharm
 # @Create time: 2/7/21 12:42 AM
-# @Description:
+# @Description: 权限装饰器
+from common.code import NotHasPermission
+
+
+def need_permission(permission_name):
+    """
+    权限检查装饰器
+    :param permission_name:
+    :return:
+    """
+
+    def check(view_func):
+        # def wrapper(*args, **kwargs):
+        def wrapper(request):
+            user = request.user
+            if user.vip.has_permission(permission_name):
+                return view_func(request)
+            else:
+                raise NotHasPermission
+
+        return wrapper
+
+    return check
