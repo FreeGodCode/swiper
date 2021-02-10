9feb4b736a30ad067131febca4c2569e15d785b5
tycarry <2501160661@qq.com>
2021-02-11 01:01:36 +0800

    添加排行榜榜单逻辑处理

--- social/helper.py
+++ social/helper.py
@@ -80,10 +80,30 @@ def users_liked_me(user):
 
 def add_swipe_score(uid, flag):
     """
-    增加滑动排行
+    增加滑动积分记录
     :param uid:
-    :param flag:
+    :param flag: like, dislike, superlike 标志
     :return:
     """
     score = {'like': 5, 'superlike': 7, 'dislike': -5}[flag]
     rds.zincrby('HotSwiped', uid, score)
+
+
+def get_top_n_swiped(num=10):
+    """获取top榜单"""
+    # 取出并清洗榜单数据
+    # zrevrange(self, name, start, end, withscores=False, score_cast_func=float)
+    origin_data = rds.zrevrange('HotSwiped', 0, num-1, withscores=True)
+    cleaned = [[int(uid), int(swiped)] for uid, swiped in origin_data]
+    # 取出用户数据
+    uid_list = [uid for uid, _ in cleaned]
+    users = User.objects.filter(id__in=uid_list)
+
+    # 将users按照uid_list的顺序排列
+    # users = sorted(iterable, key, reverse)
+    users = sorted(users, key=lambda user: uid_list.index(user.id))
+    # 整理最终结果
+    for item, user in zip(cleaned, users):
+        item[0] = user
+    return cleaned
+
